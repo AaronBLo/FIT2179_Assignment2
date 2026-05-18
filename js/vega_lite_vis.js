ef25f0fca08beaa2f6304f6592761adb20546116
AaronBLo <aloo0021@student.monash.edu>
2026-05-18 18:13:05 +1000

    Added beverage type and per capita graphs

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,3 +1,22 @@
-vegaEmbed("#alcohol_status_line_chart", "charts/alcohol_drinking_status.json", {
+var alcohol_drinking_status = "charts/alcohol_drinking_status.json";
+
+vegaEmbed("#alcohol_status_line_chart", alcohol_drinking_status, {
   actions: false
-});
+}).then(function(result) {
+}).catch(console.error);
+
+
+var beverage_2022_stacked_bar = "charts/beverage_2022_stacked_bar.json";
+
+vegaEmbed("#beverage_2022_stacked_bar", beverage_2022_stacked_bar, {
+  actions: false
+}).then(function(result) {
+}).catch(console.error);
+
+
+var per_capita_line_chart = "charts/per_capita_line_chart.json";
+
+vegaEmbed("#per_capita_line_chart", per_capita_line_chart, {
+  actions: false
+}).then(function(result) {
+}).catch(console.error);
